9d2a224dd6693b5ed3dd8c1e873e9038bf8dc8df
Nick De Villiers <noodles524@gmail.com>
2022-02-19 21:37:27 +0000

    App can now display a list of all active sessions.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,73 @@
 import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>This is the web app for my honours project.</h1>
-      </header>
-    </div>
-  );
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>This is the web app for my honours project.</h1>
+
+
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state  = {
+      sessions_list: [],
+    };
+  }
+
+  componentDidMount() {
+    this.refresh_list();
+  }
+
+  refresh_list = () => {
+    axios 
+    .get("/api/sessions")
+    .then((res) => this.setState({ sessions_list: res.data }))
+    .catch((err) => console.log(err)); 
+  };
+
+  renderItems(){
+    // this.refresh_list();
+    const newItems = this.state.sessions_list;
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span>{item.join_code}</span>
+        <span>{item.time_delay}</span>
+      </li>
+    ));
+  }
+
+  render(){
+    return(
+      <main>
+        <h1 className="text-center my-4">Sessions list</h1>
+        <div className='row'>
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+  
+
 }
 
 export default App;
