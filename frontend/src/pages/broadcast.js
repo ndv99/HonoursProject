a4bec03cff92daad3cfa295bb6ea71ddc3e4ddf3
Nick De Villiers <noodles524@gmail.com>
2022-03-28 11:32:00 +0100

    Removed Clappr player and just ask the user nicely to opoen vlc :)

--- frontend/src/pages/broadcast.js
+++ frontend/src/pages/broadcast.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ClapprPlayer from "../components/clappr-player.js";
 
 export const Broadcast = () => {
+    const link = `https://ott-video-cf.formula1.com/f7b399c8cf81ac81/out/v1/0b7a94ad4ee84b168178c6d09d38cc9c/index.m3u8?kid=1042&exp=1648483567&ttl=1440&token=bONTcXJY0huabkJCHWpbHoJjiqwngH3q0J3NOxUlqms_&start=2022-03-27T16:00:04+00:00`
     return(
         <>
-            <h1>This is the broadcast page.</h1>
-            <ClapprPlayer id="f1broadcast" source="https://ott-video-cf.formula1.com/f7b399c8cf81ac81/out/v1/0b7a94ad4ee84b168178c6d09d38cc9c/index.m3u8?kid=1042&exp=1648483567&ttl=1440&token=bONTcXJY0huabkJCHWpbHoJjiqwngH3q0J3NOxUlqms_&start=2022-03-27T16:00:04+00:00"/>
+            <h1>Hi. You should install VLC media player.</h1>
+            <p>and then open <a href={link}>this</a> with it</p>
         </>
     )
 }
