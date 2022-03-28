eb889fc5440fba4b2f47a0956cff35452c16b045
Nick De Villiers <noodles524@gmail.com>
2022-03-28 10:38:42 +0100

    Joining a session now redirects user to "waiting" page.
    Session code is stored in a cookie,

--- frontend/src/pages/waiting.js
+++ frontend/src/pages/waiting.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 export const Waiting = () => {
     return(
         <>
             <h1>Screen ready. Assign it something from your control device.</h1>
-            <p>Session ID: [session id goes here]</p>
+            <p>Session ID: {cookies.get("session_code")}</p>
         </>
     )
 }
