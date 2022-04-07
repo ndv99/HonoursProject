89a519cc7454e7b50a64eb4e56cc8be7fc44d732
Nick De Villiers <noodles524@gmail.com>
2022-04-07 22:10:08 +0100

    Converted login modal to functional component

--- frontend/src/components/f1-login-modal.js
+++ frontend/src/components/f1-login-modal.js
@@ -1,59 +1,46 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Modal, ModalBody, ModalHeader, ModalFooter, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
-class F1LoginModal extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            email: "",
-            password: ""
-        }
-    }
+function F1LoginModal (props) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    handleChange = (e) =>{
-        let { name, value } = e.target;
-        // console.log(`hey at least we're getting this far. field name is ${name} and the value is ${value}`)
-        this.setState({[name]: value})
-    }
+    const { toggle, onSave } = props;
 
-    render(){
-        const { toggle, onSave } = this.props;
-
-        return(
-            <Modal isOpen={true} toggle={toggle}>
-                <ModalHeader toggle={toggle}>Enter your F1TV Pro login details</ModalHeader>
-                <ModalBody>
-                    <Form>
-                        <FormGroup>
-                            <Label for="login-email">Email</Label>
-                            <Input 
-                                type="email"
-                                id="login-email"
-                                name="email"
-                                value={this.state.email}
-                                onChange={this.handleChange}
-                                placeholder="Email address"
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="login-password">Password</Label>
-                            <Input 
-                                type="password"
-                                id="login-password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                placeholder="Password"
-                            />
-                        </FormGroup>
-                        <Button color="danger" onClick={() => onSave(this.state.email, this.state.password)}>Log in</Button>
-                        {/* <Button color="secondary" onClick={toggle}>Cancel</Button> */}
-                    </Form>
-                </ModalBody>
-                <ModalFooter>Your login details will not be stored.</ModalFooter>
-            </Modal>
-        )
-    }
+    return(
+        <Modal isOpen={true} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Enter your F1TV Pro login details</ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label for="login-email">Email</Label>
+                        <Input 
+                            type="email"
+                            id="login-email"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email address"
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="login-password">Password</Label>
+                        <Input 
+                            type="password"
+                            id="login-password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </FormGroup>
+                    <Button color="danger" onClick={() => onSave(email, password)}>Log in</Button>
+                    {/* <Button color="secondary" onClick={toggle}>Cancel</Button> */}
+                </Form>
+            </ModalBody>
+            <ModalFooter>Your login details will not be stored.</ModalFooter>
+        </Modal>
+    )
 }
 
 export default F1LoginModal;
