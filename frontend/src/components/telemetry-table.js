6b14a70a6e4fde50d4334a626633248b4bad50bc
Nick De Villiers <noodles524@gmail.com>
2022-04-08 01:03:59 +0100

    Created a test file for fastf1. Added sample drivers to table

--- frontend/src/components/telemetry-table.js
+++ frontend/src/components/telemetry-table.js
@@ -1,6 +1,39 @@
 
 const TelemetryTable = () => {
 
+    const sample_drivers = [
+        {
+            Name: "LECLERC",
+            Initials: "LEC",
+            FullName: "C. LECLERC",
+            FirstName: "Charles",
+            LastName: "Leclerc",
+            Color: "ed1c24",
+            Team: "Ferrari",
+            Num: "16"
+        },
+        {
+            Name: "VERSTAPPEN",
+            Initials: "VER",
+            FullName: "M.VERSTAPPEN",
+            FirstName: "Max",
+            LastName: "Verstappen",
+            Color: "1e5bc6",
+            Team: "Red Bull Racing",
+            Num: "1"
+        },
+        {
+            Name: "RUSSELL",
+            Initials: "RUS",
+            FullName: "G.RUSSELL",
+            FirstName: "George",
+            LastName: "Russell",
+            Color: "6cd3bf",
+            Team: "Mercedes",
+            Num: "63"
+        },
+    ]
+
 
     return(
         <h1>This is the telemetry table.</h1>
