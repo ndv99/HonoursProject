6fc2d6b9e199704c6a6a148b9d3782cec6ef55e3
Nick De Villiers <noodles524@gmail.com>
2022-04-10 23:15:58 +0100

    Gitignore and fastf1 testing

--- server/testing-fastf1.py
+++ server/testing-fastf1.py
@@ -1,6 +1,9 @@
 import fastf1
 
-session = fastf1.get_session(2022, "Bahrain Grand Prix", "R")
-session.load()
+fastf1.Cache.enable_cache('server/fastf1cache')
+session = fastf1.get_session(2019, "Italian Grand Prix", "R")
+session.load(laps=True)
+telemetry = session.laps.pick_driver("LEC").get_telemetry()
+telemetry_json = telemetry.to_json(orient="columns")
 
-print(session)
+print(telemetry_json)
