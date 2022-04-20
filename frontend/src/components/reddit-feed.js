ce53e7464c1fee7e641f206e3111235e6c72ef41
Nick De Villiers <noodles524@gmail.com>
2022-04-20 12:06:28 +0100

    Created basic component to show comment bodies.

--- frontend/src/components/reddit-feed.js
+++ frontend/src/components/reddit-feed.js
@@ -1,8 +1,51 @@
-const RedditFeed = () => {
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { Card } from 'reactstrap'
+import LoadingSpinner from './loading-spinner.js';
 
-    return(
-        <h1>This is the Reddit feed.</h1>
-    )
+const RedditFeed = (props) => {
+
+    const [comments, setCommments] = useState()
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+
+    const before = props.before
+    const after = props.after
+
+    useEffect(() => {
+        axios.get("/api/pushshift/",
+        {headers: {
+            "link-id": 'd1ajvd',
+            'sort-by': 'created_utc',
+            'before': before,
+            'after': after
+        }})
+        .then((res) => {
+            setCommments(res.data.data);
+            console.log(comments)
+            setLoading(false)
+        })
+        .catch((err) => setError(err))
+    }, [before, after])
+
+    if (isLoading) {
+        return(
+            <LoadingSpinner />
+        )
+    } else if (error) {
+        return (
+            <div>{error}</div>
+        )
+    } else {
+        return(
+            <Card>
+                <h2>Race Megathread</h2>
+                {comments.map((comment, i) => (
+                    <div>{comment.body}</div>
+                ))}
+            </Card>
+        )
+    }
 }
 
 export default RedditFeed;
